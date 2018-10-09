5595d701b172ac77154ebd8f6cf6e87e6b0044ee
jarvis211 <Kumar.tiwari211@gmail.com>
2018-10-09 19:52:09 +0530

    first commit

---
+++ routes/index.js
@@ -0,0 +1,11 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', {
+    title: 'PSKSSSY | A Organisation for people'
+  });
+});
+
+module.exports = router;
